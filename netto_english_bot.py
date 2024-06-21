200f025d0efa242f95b5b5818c3790dd8f2b7786
Pavel <sorokin.ppp@mail.ru>
2024-06-21 23:38:53 +0800

    Final change

--- netto_english_bot.py
+++ netto_english_bot.py
@@ -49,12 +49,18 @@ else:
 
 
 def add_new_word(message):
-    new_ru_word = bot.get_updates()[-1].message.text
+    new_ru_word = message.text
     query_new_user_id = session.query(Users.id).filter(Users.uid == message.from_user.id).all()
     new_user_id = [str(i).strip("(',) ") for i in query_new_user_id]
 
-    check_word = session.query(Words.ru_word).filter(Words.ru_word.ilike(new_ru_word)).all()
-    if len(check_word) == 0:
+    check_word_in_vocabulary = (session.query(Vocabulary.id).
+                                join(Users, Users.id == Vocabulary.user_id).
+                                join(Words, Words.id == Vocabulary.word_id).
+                                filter(Users.uid == message.chat.id, Words.ru_word.ilike(new_ru_word))).all()
+
+    check_word_in_words = session.query(Words.ru_word).filter(Words.ru_word.ilike(new_ru_word)).all()
+
+    if len(check_word_in_vocabulary) == 0 and len(check_word_in_words) == 0:
         ru_to_en = translate_word(new_ru_word)
         add_word_to_words = Words(ru_word=new_ru_word, translate=ru_to_en)
         session.add(add_word_to_words)
@@ -77,12 +83,56 @@ def add_new_word(message):
         bot.send_message(message.from_user.id, f'Слово "{new_ru_word}" с переводом "{ru_to_en}" добавлено \U0001F680'
                                                f'\nОбщее количество слов в словаре = {count_words_in_vocabulary}',
                          reply_markup=markup)
-    else:
+
+    if len(check_word_in_vocabulary) == 0 and len(check_word_in_words) != 0:
+        ru_to_en = translate_word(new_ru_word)
+
+        query_new_word = session.query(Words.id).filter(Words.ru_word.ilike(new_ru_word)).all()
+        new_word_id = [str(i).strip("(',) ") for i in query_new_word]
+
+        add_all_to_vocabulary = Vocabulary(user_id=int(new_user_id[0]), word_id=int(new_word_id[0]))
+        session.add(add_all_to_vocabulary)
+        session.commit()
+
+        select_words_count = (session.query(func.count(Vocabulary.word_id)).
+                              join(Users, Users.id == Vocabulary.user_id).filter(Users.uid == message.from_user.id))
+        count_words_in_vocabulary = select_words_count.scalar()
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(Buttons.NEXT, Buttons.ADD_WORD, Buttons.DELETE_WORD)
+
+        bot.send_message(message.from_user.id, f'Слово "{new_ru_word}" с переводом "{ru_to_en}" добавлено \U0001F680'
+                                               f'\nОбщее количество слов в словаре = {count_words_in_vocabulary}',
+                         reply_markup=markup)
+
+    if len(check_word_in_vocabulary) != 0:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         markup.add(Buttons.NEXT, Buttons.ADD_WORD, Buttons.DELETE_WORD)
 
         bot.send_message(message.from_user.id, f'Слово "{new_ru_word.lower()}" уже есть в словаре.\n'
                                                f'Выбери нужную команду для продолжения \U000023EC', reply_markup=markup)
+
+
+def delete_word(message):
+    word_for_delete = message.text
+    word_id_in_vocabulary = (session.query(Vocabulary).
+                             join(Users, Users.id == Vocabulary.user_id).
+                             join(Words, Words.id == Vocabulary.word_id).
+                             filter(Users.uid == message.chat.id, Words.ru_word.ilike(word_for_delete))).first()
+
+    if word_id_in_vocabulary:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(Buttons.NEXT, Buttons.ADD_WORD, Buttons.DELETE_WORD)
+        session.delete(word_id_in_vocabulary)
+        session.commit()
+        bot.send_message(message.chat.id, f'Слово "{word_for_delete}" удалено из словаря', reply_markup=markup)
+
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(Buttons.NEXT, Buttons.ADD_WORD, Buttons.DELETE_WORD)
+        bot.send_message(message.chat.id, f'Слово "{word_for_delete}" не найдено в словаре, '
+                                          f'попробуйте выбрать другое слово.',
+                         reply_markup=markup)
 
 
 @bot.message_handler(commands=['start'])
@@ -146,7 +196,7 @@ def all_random_words(user_id):
 
 
 def true_translate(ru_word):
-    select = session.query(Words.translate).filter(Words.ru_word == ru_word).all()
+    select = session.query(Words.translate).filter(Words.ru_word.ilike(ru_word)).all()
     true_word = str(select[0]).strip("(',) ")
     return true_word
 
@@ -166,26 +216,6 @@ def next_word_button(message):
 @bot.message_handler(func=lambda message: message.text == Buttons.DELETE_WORD.text)
 def delete_word_button(message):
     bot.send_message(message.chat.id, 'Введите слово для удаления: ')
-
-    def delete_word(msg):
-        word_for_delete = bot.get_updates()[-1].message.text
-        word_id_in_vocabulary = (session.query(Vocabulary).
-                                 join(Users, Users.id == Vocabulary.user_id).
-                                 join(Words, Words.id == Vocabulary.word_id).
-                                 filter(Users.uid == msg.chat.id, Words.ru_word.ilike(word_for_delete))).first()
-
-        if word_id_in_vocabulary:
-            session.delete(word_id_in_vocabulary)
-            session.commit()
-            print(f'Слово {word_for_delete} удалено из словаря')
-
-        else:
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            markup.add(Buttons.NEXT, Buttons.ADD_WORD, Buttons.DELETE_WORD)
-            bot.send_message(msg.chat.id, f'Слово {word_for_delete} не найдено в словаре, '
-                                          f'проверьте правильность его написания.',
-                             reply_markup=markup)
-
     bot.register_next_step_handler(message, delete_word)
 
 
@@ -239,8 +269,15 @@ def correct_answer(msg, ru_word, translate_true_ru_word, en_but_1, en_but_2, en_
                          reply_markup=markup)
 
     if text == en_but_1 or text == en_but_2 or text == en_but_3:
-        bot.send_message(chat_id, f'Неправильно \U0001F622\nПопробуй ещё раз')
+        bot.send_message(chat_id, f'Неправильно \U0001F622\nПопробуйте ещё раз')
         bot.register_next_step_handler(msg, correct_answer, *words)
+
+    if text == Buttons.ADD_WORD.text:
+        add_word_button(msg)
+    if text == Buttons.NEXT.text:
+        next_word_button(msg)
+    if text == Buttons.DELETE_WORD.text:
+        delete_word_button(msg)
 
 
 bot.infinity_polling()
